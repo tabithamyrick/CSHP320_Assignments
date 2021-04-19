0095d247b34ee07ac9f93784468eaabac2ac69cb
Thad M <35581405+thadmyrick@users.noreply.github.com>
2021-04-18 20:08:26 -0500

    Assignments 1-3 Initial Commit

---
+++ CSHP320_Assigment2/Program.cs
@@ -0,0 +1,59 @@
+﻿using CSHP320_Assigment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSHP320_Assigment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Models.User> users = new List<Models.User>();
+            BuildList(users);
+            HelloPassword(users);
+            NamePasswordRemoved(users);
+            FirstHelloRemoved(users);
+            users.ForEach(WriteUsers);
+        }
+
+        private static void WriteUsers(User user)
+        {
+            Console.WriteLine(user.Name + "\n");
+        }
+
+        private static void FirstHelloRemoved(List<User> users)
+        {
+            var removeUser = users.Select(x => x).Where(x => x.Password == "hello").FirstOrDefault();
+            users.Remove(removeUser);
+        }
+
+        private static void NamePasswordRemoved(List<User> users)
+        {
+            users.RemoveAll(x => x.Password.ToLower() == x.Name.ToLower());
+        }
+
+        private static void HelloPassword(List<User> users)
+        {
+            List<User> helloItems = users.Select(x => x).Where(x => x.Password == "hello").ToList();
+            helloItems.ForEach(WriteUsers);
+        }
+
+        static void BuildList(List<Models.User> users)
+        {
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+        }
+
+        //Use System.Linq for all the requirements. IE. Don't use a for/foreach loop to manipulate the users list.
+
+        //1. Display to the console, the names of the users where the password is "hello". Hint: Where
+
+        //2. Delete any passwords that are the lower-cased version of their Name.Do not just look for "steve". It has to be data-driven.Hint: Remove or RemoveAll
+
+        //3. Delete the first User that has the password "hello". Hint: First or FirstOrDefault
+
+        //4. Display to the console the remaining users with their Name and Password.
+    }
+}
---
+++ CSHP320_Assigment3/SecondWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CSHP320_Assigment3.Models;
+
+namespace CSHP320_Assigment3
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        List<User> users = new List<User>();
+        
+        private bool NameSortAscending = true;
+        private bool PwdSortAscending = true;
+
+        public SecondWindow()
+        {
+            InitializeComponent();
+            
+            users.Add(new User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new User { Name = "Lisa", Password = "3LisaPwd" });
+
+            uxList.ItemsSource = users;
+
+            uxList.AddHandler(GridViewColumnHeader.ClickEvent, new RoutedEventHandler(uxList_OnClick));
+        }
+
+
+        private void uxList_OnClick(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader headerClicked = e.OriginalSource as GridViewColumnHeader;
+
+            uxList.ItemsSource = users; // Resets list on Click to allow for swapping between Name and Pass Column Sorting
+            if (headerClicked.Column.Header.ToString() == "Name")
+            {
+                if (NameSortAscending)
+                {
+                    uxList.ItemsSource = users.OrderBy(x => x.Name);
+                    NameSortAscending = false;
+                }
+                else
+                {
+                    uxList.ItemsSource = users.OrderByDescending(x => x.Name);
+                    NameSortAscending = true;
+                }
+            }
+            else if (headerClicked.Column.Header.ToString() == "Password")
+            {
+                if (PwdSortAscending)
+                {
+                    uxList.ItemsSource = users.OrderBy(x => x.Password);
+                    PwdSortAscending = false;
+                }
+                else
+                {
+                    uxList.ItemsSource = users.OrderByDescending(x => x.Password);
+                    PwdSortAscending = true;
+                }
+            }
+        }
+    }
+}
